a612a24eb7f14adb1c8e4b18c783595267e971a6
prarthana.bahuguna <prarthana.bahuguna.pb03@gmail.com>
2021-02-11 17:44:37 +0530

    CHANGES

--- src/component/ProfilePage.js
+++ src/component/ProfilePage.js
@@ -8,45 +8,50 @@ const ProfilePage = () => {
   const user = useContext(UserContext);
   const {photoURL, displayName, email} = user;
 
-  return (
-    <div className="container">
-      <Grid container xs={12} spacing={2} >
-        <Grid container xs={2} spacing={2} className="profileLeft">
-          {/*<div
-            style={{
-              background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
-              backgroundSize: "cover",
-              height: "200px",
-              width: "200px"
-            }}></div>*/}
-            <h2>{displayName}</h2>
-            <h3>{email}</h3>
-        </Grid>
+  return( <div>
+    <div
+    style={{
+      background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
+      backgroundSize: "cover",
+      height: "200px",
+      width: "200px"
+    }}
+    
+  ></div>
+  <div>
+    <h2>{displayName}</h2>
+    <h3>{email}</h3>
+  </div>
+<button onClick = {() => {auth.signOut()}}>Sign out</button>
 
-        <Grid container xs={8} spacing={2} >
-          <div>HELLO</div>
-        </Grid>
+</div>)
+
+  // return (
+  //   {/*<div className="container">
+  //     {/*<Grid container xs={12} spacing={2} >
+  //       <Grid container xs={2} spacing={2} className="profileLeft">
+  //         {/*<div
+  //           style={{
+  //             background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
+  //             backgroundSize: "cover",
+  //             height: "200px",
+  //             width: "200px"
+  //           }}></div>*/
+  //           /*<h2>{displayName}</h2>
+  //           <h3>{email}</h3>
+  //           <button onClick = {() => {auth.signOut()}}>Sign out</button>
+
+  //       </Grid>
+
+  //       <Grid container xs={8} spacing={2} >
+  //         <div>HELLO</div>
+  //       </Grid>
 
 
-      </Grid>
-      {/* <div>
-        <div
-          style={{
-            background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
-            backgroundSize: "cover",
-            height: "200px",
-            width: "200px"
-          }}
-          
-        ></div>
-        <div>
-        <h2>{displayName}</h2>
-        <h3>{email}</h3>
-        </div>
-      </div>
-      <button onClick = {() => {auth.signOut()}}>Sign out</button> */}
-    </div>
-  ) 
+  //         </Grid>*/
+       
+       
+  // ) 
 };
 
 export default ProfilePage;
